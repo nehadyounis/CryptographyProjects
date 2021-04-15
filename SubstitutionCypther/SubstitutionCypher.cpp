09b3a1285473f4e7d1fc16548cbd6e358ae6c7a4
Nehad Younis <nehadyounice@gmail.com>
2021-04-15 14:57:38 +0200

    .

--- SubstitutionCypther/SubstitutionCypher.cpp
+++ SubstitutionCypther/SubstitutionCypher.cpp
@@ -2,13 +2,31 @@
 
 //#include "SubstitutionCypher.h"
 
+#include <iostream>
+
+using namespace std;
+
 class SubstitutionCypher {
+    const string alpha = "abcdefghijklmnopqrstuvwxyz";
 
     public:
+
+    bool replace(string& str, const string& from, const std::string& to) {
+        size_t start_pos = str.find(from);
+        if(start_pos == string::npos)
+            return false;
+        str.replace(start_pos, from.length(), to);
+        return true;
+    }
+
     string encrypt(string plain, string key) {
-    string plain = "";
 
-
+    string crypto = "";
+    crypto = plain;
+    for (int i = 0; i <26; i++){
+    bool x = replace(crypto, alpha[i], key[i]);
+    if (!x) {crypto = "error parsing"; break;}
+    }
 
     return crypto;
 
